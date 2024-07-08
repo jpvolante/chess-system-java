9b39771b1de895e223b7680f49a3a1c7f5cbf0e9
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-07-08 17:39:42 -0300

    Fix: Special move - promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -118,7 +118,7 @@ public class ChessMatch {
             throw new IllegalStateException("Tre is no piece to be promoted");
         }
         if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getchessPosition().toPosition();
